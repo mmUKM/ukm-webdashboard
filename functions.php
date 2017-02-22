411757470ebff4ba189587c91474b91436824578
Jamaludin Rajalu <jrajalu@gmail.com>
2017-02-22 23:20:29 +0800

    No commit message

--- functions.php
+++ functions.php
@@ -118,21 +118,21 @@ add_action( 'init', 'wdash_add_revisions_support_for_pages' );
 // Add a custom user role
 
 $result = add_role( 'webmaster', __( 'Webmaster' ),
-
-array(
-
-'read' => true, // true allows this capability
-'edit_posts' => false, // Allows user to edit their own posts
-'edit_pages' => true, // Allows user to edit pages
-'edit_others_posts' => false, // Allows user to edit others posts not just their own
-'create_posts' => false, // Allows user to create new posts
-'manage_categories' => false, // Allows user to manage post categories
-'publish_posts' => false, // Allows the user to publish, otherwise posts stays in draft mode
-'edit_themes' => false, // false denies this capability. User can’t edit your theme
-'install_plugins' => false, // User cant add new plugins
-'update_plugin' => false, // User can’t update any plugins
-'update_core' => false // user cant perform core updates
-
-)
-
-);
+  array(
+    'read' => true, // true allows this capability
+    'edit_posts' => false, // Allows user to edit their own posts
+    'edit_pages' => true, // Allows user to edit pages
+    'edit_others_posts' => false, // Allows user to edit others posts not just their own
+    'edit_others_pages' => false,
+    'edit_published_pages' => false,
+    'delete_others_pages' => false,
+    'delete_published_pages' => false,
+    'create_posts' => false, // Allows user to create new posts
+    'manage_categories' => false, // Allows user to manage post categories
+    'publish_posts' => false, // Allows the user to publish, otherwise posts stays in draft mode
+    'publish_pages' => false,
+    'edit_themes' => false, // false denies this capability. User can’t edit your theme
+    'install_plugins' => false, // User cant add new plugins
+    'update_plugin' => false, // User can’t update any plugins
+    'update_core' => false // user cant perform core updates
+  ) );
--- header.php
+++ header.php
@@ -7,3 +7,5 @@
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
   <?php wp_head(); ?>
 </head>
+<body>
+<?php get_search_form(); ?>
